9f07da88aa1b688926061bb03c48cb1a6b66f60a
Vinicius Silva <vinicius.luiz19@gmail.com>
2021-05-16 13:53:59 -0300

    feat: basic project setup

---
+++ src/main/java/br/edu/ifsp/scl/pipegene/PipegeneApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.scl.pipegene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipegeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipegeneApplication.class, args);
+	}
+
+}
