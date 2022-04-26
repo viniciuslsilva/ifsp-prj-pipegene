b27dc5a292626047f477e9662899c2d49df4a275
Vinicius Silva <vinicius.luiz@ifood.com.br>
2022-04-26 01:01:01 -0300

    update README.md and add docker-compose

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 ifsp-prj-pipegene
+
+
+O banco já está está sendo populado com alguns dados para facilitar os testes.
+
+É criado um usuário com username "vini" e "senha password".
+
+Ao todo é criado 3 providers/serviços que representam as aplicações em python:
+
+1 - O primeiro é o "Grafico pré processamento" com PROVIDER_ID="78cec5db-6396-4fd9-803f-1fd469d76312", ele representa
+a app python "app_grafico_pre_processamento.py" que roda na porta 5011. Já existe um projeto chamado
+"Explorando pré processamentos MAF" (f2d6a949-8bb5-4df5-8ca7-e5b8d2292488) o qual possui um dataset e uma pipeline configurada
+para chamar esse serviço. Ele é o mais simples, recebe um maf e devolve um png.
+
+2 - O segundo é o "Pré processamento - Rodrigo" com PROVIDER_ID="e8bf42e4-2ffc-4935-a546-ee5d9263f419", ele representa
+a app python "app_pre_processamento_output_maf.py" que roda na porta 5001. A ideia dele é receber um maf de input
+e como output devolver um maf, que sera utilizado como input para outro serviço. 
+
+3 - O terceiro é o "Classificação de variante - Rodrigo" com PROVIDER_ID="49df4595-b8af-4e32-8791-65e583ae08a2", ele 
+representa a app python "app_classificacao_variant.py" que roda na porta 5002. A ideia dele é receber um maf de input
+resultante de um pre processamento e devolver um png com a classificação da variant.
+
+
+Na pasta docker tem um arquivo chamado docker-compose.yml ele simplifica para subir o container do postgres, basta a partir
+da raiz do projeto rodar:
+
+```
+cd docker
+docker-compose up --build --remove-orphans -d
+```
+
+Ainda na pasta docker, entrando em postgres/sql/V002__populate_schema.sql tem os scripts para popular a base
+com os serviços descritos acima. Lembrando que os ids dos providers são colocados hardcode nas apps python.
+
+Para subir o serviços em python siga as instruções no outro readme.
--- docker/postgres/sql/V002__populate_schema.sql
+++ docker/postgres/sql/V002__populate_schema.sql
@@ -1,10 +1,17 @@
 \connect "pipegine"
 
+
+
+
+-- Cria um usuário com username 'vini' e senha 'password'
+
 insert into pipegine_platform.application_user(id, name, username, password, is_account_non_expired,
     is_account_nonLocked, is_credentials_non_expired, is_enabled)
     values ('78cec5db-6396-4fd9-803f-1fd469d76330'::uuid, 'vini', 'vini',
         '$2a$10$M2lu6nBJ4yCdJ/qtbT7aZeofmxsFqYlMQtz1M2QQgRm1sCwTi2i/m', true, true, true, true);
 
+
+-- cria provider para grafico pre processamento
 insert into pipegine_platform.provider(id, name, description, url, input_supported_types, output_supported_types,
                                        owner_id, operations)
     values('78cec5db-6396-4fd9-803f-1fd469d76312'::uuid, 'Grafico pré processamento',
@@ -23,21 +30,23 @@ insert into pipegine_platform.provider(id, name, description, url, input_support
             ]
         }]'::jsonb));
 
+
+-- cria projeto que vai utilizar do provider de Grafico pré processamento
 insert into pipegine_platform.project(id, name, description, owner_id)
     values ('f2d6a949-8bb5-4df5-8ca7-e5b8d2292488'::uuid, 'Explorando pré processamentos MAF',
             'Analise de gráficos resultante de pre processamentos de arquivos MAF',
             '78cec5db-6396-4fd9-803f-1fd469d76330'::uuid);
 
+-- cria dataset do projeto que servira de input para o Grafico pré processamento
 insert into pipegine_platform.dataset(id, filename, project_id)
     values('042e4e39-ba0b-49d1-a01a-237333d0b1a5'::uuid,
            '042e4e39-ba0b-49d1-a01a-237333d0b1a5_uploads_mock_GBM_MEMo.maf', 'f2d6a949-8bb5-4df5-8ca7-e5b8d2292488'::uuid);
 
-
-
+-- cria pipeline para o Grafico pré processamento
 INSERT INTO pipegine_platform.pipeline(id, project_id, description)
     VALUES('959f2ff3-ed29-435b-83bb-c8390f6385bf'::uuid, 'f2d6a949-8bb5-4df5-8ca7-e5b8d2292488'::uuid, 'Exporta grafico resultante do pre processamento');
 
-
+-- cria step unico para o Grafico pré processamento
 INSERT INTO pipegine_platform.pipeline_step(step_id, pipeline_id, provider_id, input_type, output_type, params, step_number)
     VALUES('715d7d76-b5d1-4d45-b1f6-66b46d3c2964'::uuid, '959f2ff3-ed29-435b-83bb-c8390f6385bf'::uuid,
         '78cec5db-6396-4fd9-803f-1fd469d76312'::uuid, 'maf', 'png',
@@ -45,6 +54,8 @@ INSERT INTO pipegine_platform.pipeline_step(step_id, pipeline_id, provider_id, i
            1);
 
 
+
+-- cria novo provider que realiza o pre processamento exportando um maf que sera utilizado de input
 insert into pipegine_platform.provider(id, name, description, url, input_supported_types, output_supported_types,
                                        owner_id, operations)
 values('e8bf42e4-2ffc-4935-a546-ee5d9263f419'::uuid, 'Pré processamento - Rodrigo',
@@ -63,6 +74,7 @@ values('e8bf42e4-2ffc-4935-a546-ee5d9263f419'::uuid, 'Pré processamento - Rodri
             ]
         }]'::jsonb));
 
+-- cria novo provider que recebera como input o maf resultando do pre processamento e fara a classificação da variante
 insert into pipegine_platform.provider(id, name, description, url, input_supported_types, output_supported_types,
                                        owner_id, operations)
 values('49df4595-b8af-4e32-8791-65e583ae08a2'::uuid, 'Classificação de variante - Rodrigo',
@@ -80,3 +92,30 @@ values('49df4595-b8af-4e32-8791-65e583ae08a2'::uuid, 'Classificação de variant
                 }
             ]
         }]'::jsonb));
+
+
+-- cria projeto que vai utilizar do provider de Pré processamento - Rodrigo e Classificação de variante - Rodrigo
+INSERT INTO pipegine_platform.project (id, name, description, owner_id)
+VALUES ('28559f11-0c41-4444-a087-c14efde8b482', 'Pré processamento - Rodrigo e Classificação de variante - Rodrigo',
+        'Recebe um input e devolve um maf para ser utilizado como input em outro serviço',
+        '78cec5db-6396-4fd9-803f-1fd469d76330');
+
+-- cria dataset do projeto que servira de input para o Pré processamento - Rodrigo
+INSERT INTO pipegine_platform.dataset (id, filename, project_id)
+VALUES ('e1a8ba35-d029-4a7c-bb98-ae7e84c41a41',
+        'e1a8ba35-d029-4a7c-bb98-ae7e84c41a41_uploads_BRCA2_data_mutations_extended.maf',
+        '28559f11-0c41-4444-a087-c14efde8b482');
+
+-- cria pipeline para o Pré processamento - Rodrigo e Classificação de variante - Rodrigo
+INSERT INTO pipegine_platform.pipeline (id, project_id, description)
+VALUES ('3677aa2e-6be8-43f9-ad8c-a0f1f71e2040', '28559f11-0c41-4444-a087-c14efde8b482', 'Realiza pre processamento e faz a classificação de variante - Rodrigo');
+
+
+-- cria steps para o Pré processamento - Rodrigo e Classificação de variante - Rodrigo
+INSERT INTO pipegine_platform.pipeline_step (step_id, pipeline_id, provider_id, input_type, output_type, params, step_number)
+VALUES ('7b97b9b1-346b-47ed-aeef-bee8bbdcf064', '3677aa2e-6be8-43f9-ad8c-a0f1f71e2040', 'e8bf42e4-2ffc-4935-a546-ee5d9263f419',
+        'maf', 'maf', '{"columns": "Hugo_Symbol,Chromosome,Start_Position,End_Position,Reference_Allele,Tumor_Seq_Allele2,Variant_Classification,Variant_Type,Tumor_Sample_Barcode"}', 1);
+
+INSERT INTO pipegine_platform.pipeline_step (step_id, pipeline_id, provider_id, input_type, output_type, params, step_number)
+ VALUES ('2fe129d3-f3d2-46c5-b34a-e472ed217b07', '3677aa2e-6be8-43f9-ad8c-a0f1f71e2040', '49df4595-b8af-4e32-8791-65e583ae08a2',
+         'maf', 'png', '{"columns": "Hugo_Symbol,Chromosome,Start_Position,End_Position,Reference_Allele,Tumor_Seq_Allele2,Variant_Classification,Variant_Type,Tumor_Sample_Barcode"}', 2);
